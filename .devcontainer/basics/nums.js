9521a179dee0235c17a963b04ccc977808c47f16
Raksha H G <145531180+Raksha-HG@users.noreply.github.com>
2026-03-04 12:51:04 +0000

    nums and math

---
+++ .devcontainer/basics/nums.js
@@ -0,0 +1,29 @@
+const score=90  
+
+const balance= new Number(700);
+
+console.log(balance)
+console.log(balance.toString()) 
+
+const otherNumber=29.8966
+console.log(otherNumber.toPrecision(4))
+
+const hundreds=100000000
+console.log(hundreds.toLocaleString())
+
+
+//******************************MATHS*************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4) );
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+
+//RANDOM --B/W 0 & 1
+console.log((Math.random()*10)+1)
+
+const min=10
+const max= 20
+
+(console.log((Math.random() *(max-min+1) )+ min))
